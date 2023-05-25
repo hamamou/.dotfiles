3bf1200f3d3ae3c97b25f3212ecd51fa2f271354
Houssem Amamou <hamamou@coveo.com>
2023-05-25 13:10:36 -0400

    add back lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -2,5 +2,13 @@ require('lualine').setup{
     options= {
         component_separators = '',
         section_separators = '',
-    }
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff'},
+        lualine_c = {{'filename', path = 1}},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {'location'}
+    },
 }
